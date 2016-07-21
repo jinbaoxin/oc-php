33e3c231937ea248c12efb89c40036fe3d293584
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-07-21 16:05:54 -0700

    Update dbcheck.php

--- dbcheck.php
+++ dbcheck.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = $_ENV['MYSQL_SERVICE_HOST'];
+$username = $_ENV['MYSQL_USER'];
+$password = $_ENV['MYSQL_PASSWORD'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -9,6 +9,8 @@ $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    $db_selected = mysql_select_db($_ENV['MYSQL_DATABASE'], $conn);
 } 
+
 echo "Connected successfully";
 ?>
