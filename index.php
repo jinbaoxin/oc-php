c7a10691006676523fb112315168794b2741a1a6
Chakradhar Rao Jonagam <debianmaster@users.noreply.github.com>
2016-07-21 14:37:04 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-
-<html>
-<body style="width:80%;margin-left:auto;margin-right:auto;">
-        
-        
   <?php
     echo "My Host Name ".gethostname(). " App version 1"
   ?>
-  
-  
-  
-</body>
-</html>
